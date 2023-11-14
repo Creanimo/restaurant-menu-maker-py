87fb0d5cb5d8125f4ff25a8ca31f9bc983729591
Ferdinand Engländer <ferdinand@creanimo.net>
2023-11-14 21:58:29 +0100

    remove hash and use frozen=true instead

--- restaurantmenu/substance.py
+++ restaurantmenu/substance.py
@@ -6,14 +6,6 @@ class Substance:
     name: str
     inline_icon: str
 
-    # A method that returns the hash value of the substance object
-    def __hash__(self) -> int:
-        return hash(self.name)
-
-    # A method that compares the substance object with another object for equality
-    def __eq__(self, other) -> bool:
-        return isinstance(other, Substance) and self.name == other.name
-
 # A class that represents an allergen, which is a subclass of substance
 @dataclass(frozen=True)
 class Allergen(Substance):
